91801ddc8f7b5d522551b8745117dc1c25046819
Circle <45083793+Chenjiang-circle@users.noreply.github.com>
2020-03-31 20:12:05 +0800

    Update TaskOne.java

--- Assignment/FirstWeek/src/com/lab515/Task/TaskOne.java
+++ Assignment/FirstWeek/src/com/lab515/Task/TaskOne.java
@@ -6,21 +6,6 @@
 public class TaskOne {
 
     public static void main(String[] args) {
-			Scanner a = new Scanner(System.in); 
-			String str = a.nextLine();
-			String[] strs = str.split(" ");
-			for (int i = strs.length - 1;i>=0;i--)
-			{
-				if(i!=0)
-				{
-					System.out.print(strs[i] + " ");
-				}
-				else
-				{
-					System.out.println(strs[i]);
-				}
-			}
-			a.close();
 
     }
 }
