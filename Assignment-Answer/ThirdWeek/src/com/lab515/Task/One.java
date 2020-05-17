1f1fca5d688f2bb24f9422548f7fdfd7a09dd54d
SL0208 <1047744051@qq.com>
2020-05-17 23:07:26 +0800

    update

---
+++ Assignment-Answer/ThirdWeek/src/com/lab515/Task/One.java
@@ -0,0 +1,28 @@
+﻿package com.lab515.Task;
+
+/**
+ * 被 3 整除（Be divided exactly by 3）
+ */
+
+import java.util.Scanner;
+
+public class One {
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int start = s.nextInt();
+		int end = s.nextInt();
+		if(start > end)
+		{
+			System.out.println(0);
+			return;
+		}
+		int cnt = 0;
+		for(long i = start; i <= end; i++)
+		{
+			if(i%3 == 0 || i%3 == 2)
+				cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
